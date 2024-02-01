f39fbd580765204c10a6618419d8bd2aab02b86b
Asianofthesouth <156933561+Asianofthesouth@users.noreply.github.com>
2024-01-31 22:51:03 -0500

    2D Platformer Template

---
+++ PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState {Idle, Running, Jumping, Falling};
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb =  GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimation();
+    }
+    private void UpdateAnimation()
+    {
+        MovementState state;
+        if (dirX > 0f)
+        {
+            state = MovementState.Running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.Running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.Idle;
+        }
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.Jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.Falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+}
